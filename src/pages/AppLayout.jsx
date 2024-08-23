3c87de761b519f883090782875a8ee313bd5d45d
Anas Hany <anashany219@gmail.com>
2024-08-23 18:46:19 +0300

    🎨 Design and build the app layout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,12 @@
-import AppNav from "../components/AppNav";
+import Map from "../components/Map";
+import styles from "./AppLayout.module.css";
+import Sidebar from "../components/Sidebar";
 
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      <h1>AppLayout</h1>
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
