2f3a5bf6a2857a6970fa79b0430ca33ce1132670
Anas Hany <anashany219@gmail.com>
2024-08-31 23:03:43 +0300

    ⚡ Optimize bundle size with code splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,66 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import Login from "./pages/Login";
-import City from "./components/City";
-import Form from "./components/Form";
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import Homepage from "./pages/Homepage";
-import AppLayout from "./pages/AppLayout";
-import CityList from "./components/CityList";
-import PageNotFound from "./pages/PageNotFound";
-import CountryList from "./components/CountryList";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/FakeAuthContext";
 import ProtectedRoute from "./pages/ProtectedRoute";
+
+import City from "./components/City";
+import Form from "./components/Form";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+
+// import Login from "./pages/Login";
+// import Product from "./pages/Product";
+// import Pricing from "./pages/Pricing";
+// import Homepage from "./pages/Homepage";
+// import AppLayout from "./pages/AppLayout";
+// import PageNotFound from "./pages/PageNotFound";
+
+// dist/assets/index-NJNJteL1.css   30.50 kB │ gzip:   5.13 kB
+// dist/assets/index-BaMmyEcn.js   508.50 kB │ gzip: 148.74 kB
+
+// dist/assets/index-wHf-CsFN.css           26.30 kB │ gzip:   4.37 kB
+// dist/assets/index-B9T5cSvX.js           349.81 kB │ gzip: 102.08 kB
+
+const Login = lazy(() => import("./pages/Login"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Homepage = lazy(() => import("./pages/Homepage"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
 function App() {
   return (
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
-          <Routes>
-            <Route index element={<Homepage />} />
-            <Route path="product" element={<Product />} />
-            <Route path="pricing" element={<Pricing />} />
-            <Route path="login" element={<Login />} />
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route index element={<Homepage />} />
+              <Route path="product" element={<Product />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="login" element={<Login />} />
 
-            <Route
-              path="app"
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
-              <Route index element={<Navigate replace to="cities" />} />
-              <Route path="cities" element={<CityList />} />
-              <Route path="cities/:id" element={<City />} />
-              <Route path="countries" element={<CountryList />} />
-              <Route path="form" element={<Form />} />
-            </Route>
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
 
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </AuthProvider>
